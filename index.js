39c58fc4101047d32c3792fef7b073a8c1d16a1f
Adam Chambers <adammchambers@gmail.com>
2018-02-11 21:35:39 -0500

    Poll ETH price in USD, set Hue light to green if price goes up and red if price goes down

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-const Hue = require("node-hue-api")
+const fetch = require('node-fetch')
+const Hue = require('node-hue-api')
+
+const ethPriceAPI = 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD'
+
 const HueApi = Hue.HueApi
 const HueLightState = Hue.lightState
 
 const host = "192.168.0.24"
 const username = "xrvO5QZn9lqCGs4DLzZbi2AtFZDtMXtb1--50JCw"
-const hue = new HueApi(host, username)
-let state
+const light = 4
 
-const log = data => {
-  console.log(JSON.stringify(data, null, 2));
+const hue = new HueApi(host, username)
+
+let state = null
+let currentPrice = 0
+
+const ethPrice = first => {
+  fetch(ethPriceAPI).then(response => response.json()).then(data => {
+    if (first) {
+      currentPrice = data.USD
+      console.log(currentPrice)
+
+    } else if (currentPrice !== data.USD) {
+      const dir = currentPrice > data.USD ? 'down' : 'up'
+      const rgb = dir === 'up' ? [0, 255, 0] : [255, 0, 0]
+
+      currentPrice = data.USD
+      state = HueLightState.create().on().rgb(...rgb)
+
+      hue.setLightState(light, state, function(err, lights) {
+        if (err) {
+          console.log(JSON.stringify(err, null, 2))
+        }
+      })
+
+      console.log(`Price has gone ${dir}!`, currentPrice)
+    }
+  })
 }
 
-state = HueLightState.create().on().white(500, 70)
+const init = () => {
+  ethPrice(true)
+  setInterval(ethPrice, 1000)
+}
 
-hue.setLightState(4, state, function(err, lights) {
-  if (err) {
-    log(err)
-    return
-  }
-
-  log(lights)
-})
+init(true)
